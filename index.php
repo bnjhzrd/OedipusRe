1eb029453262bd36c7bc3d90f7036ad67bf60375
bnjhzrd <bnjhzrd@gmail.com>
2017-08-23 22:48:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
 
 
 
-   <h2>Oedipus <span class=me>Rex</span>.</h2><br>??<a class="me" href="">Video</a></h3>
- </body>
+   <h2>Oedipus <span class=me>Rex</span>.</h2><
+   
+   video width="320" height="240" controls>
+  <source src="movie.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video>
+   
+ </body> 
 </html>
