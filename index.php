f76a21c79cbfee097737aca76732f6de1258ff76
bnjhzrd <bnjhzrd@gmail.com>
2017-08-24 16:11:46 +0300

    width=900

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
 
    <h2>Oedipus <span class=me>Rex</span>.</h2>
    <h3>Prometheus performance in Kea town hall</h3>  
-<iframe width="900" height="500" src="https://www.youtube.com/embed/rAc8fnvjXbM?autoplay=1">  </iframe>   
+<iframe width="900" height="600" src="https://www.youtube.com/embed/rAc8fnvjXbM?autoplay=1">  </iframe>   
  </body> 
 </html>
