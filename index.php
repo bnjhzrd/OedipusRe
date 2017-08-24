f4f8bfe6655bfd482665066196d70d74e2897b1e
bnjhzrd <bnjhzrd@gmail.com>
2017-08-24 11:50:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
    <h2>Oedipus <span class=me>Rex</span>.</h2>
    
    
-   <source width="800" height="500" autoplay>
+   <audio width="800" height="500" autoplay>
    <audio controls>
   <source src="VIDEO_TS.mp4" type="audio/mp4">
   </audio>
