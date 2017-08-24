be1fd9c8498fc18c3eb0baa5091f932d9ae5afce
bnjhzrd <bnjhzrd@gmail.com>
2017-08-24 11:38:14 +0300

    make the video is play

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
    <h2>Oedipus <span class=me>Rex</span>.</h2>
    
-   <video width="800" height="500" controls>
+   <video width="900" height="500" controls>
   <source src="video_ts.mp4" type="video/mp4">
 Your browser does not support the video tag.
 </video>
