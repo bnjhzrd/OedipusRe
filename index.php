0d4b18ac119018b959943e8b0a48e26b56f21c9b
bnjhzrd <bnjhzrd@gmail.com>
2017-08-24 11:45:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,11 @@
 
    <h2>Oedipus <span class=me>Rex</span>.</h2>
    
-   <video width="900" height="500" controls>
-  <source src="video_ts.mp4" type="video/mp4">
-Your browser does not support the video tag.
-</video>
+   <audio controls>
+  <source src="VIDEO_TS.mp4" type="audio/mp4">
+ 
+  Your browser does not support the audio element.
+</audio>
    
  </body> 
 </html>
