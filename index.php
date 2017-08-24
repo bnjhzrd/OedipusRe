1d762de730c1fea5f2063828d4feeab0c36c7dbb
bnjhzrd <bnjhzrd@gmail.com>
2017-08-24 11:29:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
    
    <video width="800" height="500" controls>
   <source src="movie.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
 Your browser does not support the video tag.
 </video>
    
