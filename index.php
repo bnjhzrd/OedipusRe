1d1f221b15ee2f0412dd87df1e868ebca6d8e57c
bnjhzrd <bnjhzrd@gmail.com>
2017-08-24 16:36:32 +0300

    make the video is big

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
 
    <h2>Oedipus <span class=me>Rex</span>.</h2>
    <h3>Prometheus performance in Kea town hall</h3>  
-<iframe width="800" height="600" src="https://www.youtube.com/embed/rAc8fnvjXbM?autoplay=1">  </iframe>   
+<iframe width="800" height="500" src="https://www.youtube.com/embed/rAc8fnvjXbM?autoplay=1">  </iframe>   
  </body> 
 </html>
