5d3a77fa3b14c7779e8bc3635cb89c374da6783a
bnjhzrd <bnjhzrd@gmail.com>
2017-08-25 12:19:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
    <h2>Oedipus <span class=me>Rex</span>.</h2>
    <h3>Prometheus performance in Kea town hall</h3>  
 <iframe width="900" height="500" src="https://www.youtube.com/embed/rAc8fnvjXbM?autoplay=1">  </iframe>
-  <script src="Paragraphs.md"></script>
+  
   
 <body> <p style="color:yellow">Domodocos is from  Sorbonne and Rouen universites.</p>
     <body> <p style="color:yellow">Play the prometheus in Kea at Town Hall.</p>
-
+          
  </body> 
 </html>
