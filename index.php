67bebcd4f6a3d62d4e198b6e68b74c68a17ef970
bnjhzrd <bnjhzrd@gmail.com>
2017-08-25 13:20:43 +0300

    make is font is yellow

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
-<html>     
+<html>
 <head>
 <link rel = stylesheet type=text/css href=my.css>
 </head>
 <body bgcolor="#F65F8=">
 
-  <object width="100%" height="500px" data="snippet.html"></object> 
+  <object width="100%" height="500px" data="snippet.html"></object>
 
    <h2>Oedipus <span class=me>Rex</span>.</h2>
-   <h3>Prometheus performance in Kea town hall</h3>  
+   <h3>Prometheus performance in Kea town hall</h3>
 <iframe width="900" height="500" src="https://www.youtube.com/embed/rAc8fnvjXbM?autoplay=1">  </iframe>
-  
-  
+
+
 <body> <p style="color:yellow">Domodocos is from  Sorbonne and Rouen universites.</p>
+
+
+            <body> <p style="color:yellow"> The play is french and greek </p>   
+
     <body> <p style="color:yellow">Play the prometheus in Kea at Town Hall.</p>
-          
- </body> 
+
+ </body>
 </html>
