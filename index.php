6445fbc8dfb7698a780295179cc46691cc2c62c1
bnjhzrd <bnjhzrd@gmail.com>
2017-12-16 04:15:19 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 <link rel = stylesheet type=text/css href=my.css>
 </head>
 <body bgcolor="#F65F8=">
-
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+  
   <object width="100%" height="500px" data="snippet.html"></object>
 
    <h2>Oedipus <span class=me>Rex</span>.</h2>
@@ -12,7 +17,11 @@
 
 
 <body> <pre style="color:yellow">Domodocos is from Sorbonne and Rouen universites.
-
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
           The play is French and Greek.
 </pre>
 
